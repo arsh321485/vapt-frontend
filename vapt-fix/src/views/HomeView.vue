a341cbab4703671516c435f1fa76ba29838042d2
sakshi <sakshi@secureitlab.info>
2025-07-08 17:53:59 +0530

    vue-setup

--- vapt-fix/src/views/HomeView.vue
+++ vapt-fix/src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="section hero-section">
+      <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6">
+          <p><i class="bi bi-shield-fill-check"></i> Vetted, tested security. Trusted by experts</p>
+          <h1>Secure your assets with proven fixes and clear steps.</h1>
+          <p class="hero-text">Security isn’t just about finding flaws—it’s about fixing them fast. We close the gaps that reports leave behind, giving you lasting protection.</p>
+          <button type="button" class="btn hero-btn">Fix your vulnerabilities now <i class="bi bi-arrow-right-circle-fill fs-5 ms-1"></i></button>
+        </div>
+        <div class="col-lg-6">
+          <!-- <div class="hero-img">
+            <img src="../assets/images/hero-img.png" alt="">
+          </div> -->
+          <div class="cyber-bg">
+            <img src="../assets/images/hero-img.png" alt="Cyber Security" class="center-img" />
+          </div>
+        </div>
+      </div>
+     </div>
+    </div>
+     
   </main>
 </template>
+
+
