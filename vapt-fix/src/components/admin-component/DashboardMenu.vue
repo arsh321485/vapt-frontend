066a00eeeaa038f707bd2b6e5b65529ed0704eae
sakshi <sakshi@secureitlab.info>
2025-09-24 15:22:04 +0530

    admin-developerscreenupdated

--- vapt-fix/src/components/admin-component/DashboardMenu.vue
+++ vapt-fix/src/components/admin-component/DashboardMenu.vue
@@ -7,7 +7,7 @@
       </div> -->
 
       <!-- Navigation Icons -->
-      <div class="flex-grow-1 d-flex flex-column align-items-center  text-center pt-4 mb-3">
+      <div class="flex-grow-1 d-flex flex-column align-items-center  text-center pt-5 mb-3">
          <div>
           <router-link to="/admindashboardonboarding">
           <button class="btn border-0" :class="{ activeBtn: isActive('/admindashboardonboarding') }"><i class="bi bi-house-door nav-menu"></i>
